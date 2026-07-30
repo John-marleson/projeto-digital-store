64d6a0c7ffa5737d1430bc7f62f71464964e4413
John-marleson <johnmarlesom67@gmail.com>
2026-07-29 23:29:15 -0300

    middleware de autenticação de token

---
+++ project-root/src/middlewares/autenticacaoMiddleware.js
@@ -0,0 +1,21 @@
+require('dotenv').config({path: '../.env'})
+const jwt = require('jsonwebtoken')
+
+async function autenticacaoMiddleware(req, res, next) {
+    try{
+        const token = req.headers.authorization.split(' ')[0]
+        const verify = await jwt.verify(token, process.env.PALAVRA_SECRETA_JWT)
+        if(!verify){
+            return res.status(401).json({
+                erro: 'token invalido'
+            })
+        }
+
+        req.infoUsuario = verify
+        next()
+    }catch(erro){
+        return res.status(500).json
+    }
+}
+
+module.exports = autenticacaoMiddleware
